41ca9661a2fd8c927ed5efb03f28b91c205da371
RaeinF <rf1381@gmail.com>
2022-04-11 02:39:07 +0430

    Finishing the Game but should get cleaner and add ui and get faster and with more random things..

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-from enum import Flag
+
 import sys
-from tabnanny import check
 print(sys.setrecursionlimit(40000))
 
 
 
 
-GameMap=[ ["X", "*", "O"]
-         ,["*", "O","*"]
-         ,["*", "*", "X"]]
+GameMap=[ ["*", "*", "*"]
+         ,["*", "*", "*"]
+         , ["*", "*", "*"]
+         ]
 
 def Map_Changer(Map,Turn,po):
     m=[]
@@ -21,11 +21,11 @@ def Map_Changer(Map,Turn,po):
 
 def Check_Win(GameMap):
     for i in range(3):
-        if GameMap[i][0] == GameMap[i][1] == GameMap[i][2]:
+        if GameMap[i][0] == GameMap[i][1] == GameMap[i][2] and GameMap[i][0] !="*":
             return GameMap[i][0]
-        if GameMap[0][i] == GameMap[1][i] == GameMap[2][i]:
+        if GameMap[0][i] == GameMap[1][i] == GameMap[2][i] and GameMap[2][i] != "*":
             return GameMap[0][i]
-    if GameMap[2][0]== GameMap[1][1] == GameMap[0][2] or GameMap[2][2]==GameMap[1][1]==GameMap[0][0]:
+    if (GameMap[2][0] == GameMap[1][1] == GameMap[0][2] or GameMap[2][2] == GameMap[1][1] == GameMap[0][0]) and GameMap[1][1] != "*":
         return GameMap[1][1]
     draw=0
     for i in GameMap:
@@ -39,7 +39,10 @@ def Check_Win(GameMap):
 def Ai():
     global GameMap
     Xwins=[]
+    TricWin=[]
+    Draws=[]
     def Think(Map,route=[]):
+        Flag=None
         for i in range(3):
             for j in range(3):
                 if Map[i][j] == "*":
@@ -48,39 +51,83 @@ def Ai():
                     r.append((i, j, "X"))
                     if Check_Win(NewMap) == "X":
                         Xwins.append(route+r)
-        for i in range(3):
-            for j in range(3):
-                if Map[i][j] =="*":
-                    r=[]
-                    NewMap=Map_Changer(Map,"X",(i,j))
-                    r.append((i,j,"X"))
-                    Flag=True
-                    for q in range(3):
-                        for w in range(3):
-                            if NewMap[q][w] == "*":
-                                r2=[]
-                                NewMap2 = Map_Changer(NewMap, "O", (q, w))
-                                r2.append((q, w, "O"))
-                                if Check_Win(NewMap2) =="O":
-                                    Flag=False
-                                    break
-                    if Flag:
-                        for q in range(3):
-                            for w in range(3):
-                                if NewMap[q][w] == "*":
-                                    r2 = []
-                                    NewMap2 = Map_Changer(NewMap, "O", (q, w))
-                                    r2.append((q, w, "O"))
-                                    Think(NewMap2, route+r+r2)
-                                
-                        
-       
+                        Flag=True
+        if Flag !=True:           
+            for i in range(3):
+                for j in range(3):
+                    if Map[i][j] =="*":
+                        r=[]
+                        NewMap=Map_Changer(Map,"X",(i,j))
+                        r.append((i,j,"X"))
+                        Flag=Check_Win(NewMap)
+                        if Flag=="Draw":
+                            Draws.append(route+r)
+                        if Flag !="Draw":
+                            for q in range(3):
+                                for w in range(3):
+                                    if NewMap[q][w] == "*":
+                                        r2=[]
+                                        NewMap2 = Map_Changer(NewMap, "O", (q, w))
+                                        r2.append((q, w, "O"))
+                                        if Check_Win(NewMap2) =="O":
+                                            Flag=False
+                        if Flag != False and Flag != "Draw":
+                            abselotwin = True
+                            for q in range(3):
+                                for w in range(3):
+                                    if NewMap[q][w] == "*":
+                                        r2 = []
+                                        NewMap2 = Map_Changer(NewMap, "O", (q, w))
+                                        r2.append((q, w, "O"))
+                                        a=Think(NewMap2, route+r+r2)
+                                        if a == False or a == "Draw" :
+                                            abselotwin = False
+                                            Flag=False
+                    
+                            if abselotwin:
+                                TricWin.append(route+r)
+                               
+        return Flag            
+        
                         
          
      
     Think(GameMap)
-    for i in Xwins:
+    TricWin = sorted(TricWin, key=len)
+    Xwins = sorted(Xwins, key=len)
+
+    # for i in TricWin:
+    #     print(*i)
+    # print(len(Xwins),len(TricWin),len(Draws))
+    if len(TricWin) !=0:
+        GameMap=Map_Changer(GameMap, "X", (TricWin[0][0][0], TricWin[0][0][1]))
+
+    elif len(Xwins) != 0:
+        GameMap=Map_Changer(GameMap, "X", (Xwins[0][0][0], Xwins[0][0][1]))
+    else:
+        GameMap=Map_Changer(GameMap, "X", (Draws[0][0][0], Draws[0][0][1]))
+    
+
+while True:
+    Ai()
+    for i in GameMap:
         print(*i)
-Ai()
-# for i in GameMap:
-#     print(*i)
+    if Check_Win(GameMap) !=None:
+        
+        print("game finished")
+        print("the result is:",Check_Win(GameMap))
+        
+        break
+    print("-----------")
+    print("Your Turn")
+    x=int(input("x"))
+    y=int(input("y"))
+    GameMap=Map_Changer(GameMap, "O", (x, y))
+    for i in GameMap:
+        print(*i)
+    if Check_Win(GameMap) != None:
+
+        print(Check_Win(GameMap))
+        print("game finished")
+        break
+    print("-----------")
